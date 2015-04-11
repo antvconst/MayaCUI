5ca328bb333e4e82ca416b4cbb514864fc94cd46
Anton Konstantinov <falceeffected@gmail.com>
2015-04-11 21:01:24 +0300

    Implemented update script

--- __init__.py
+++ __init__.py
@@ -1,11 +1,36 @@
+import cuiViewer
+import cuiBuilder
+
+BUILDER_INSTANCE = None
+VIEWER_INSTANCE = None
+
 def builder():
-	import cuiBuilder
-	reload(cuiBuilder)
-	builder = cuiBuilder.CUIBuilder()
-	builder.show()
+  global BUILDER_INSTANCE
+  reload(cuiBuilder)
+  BUILDER_INSTANCE = cuiBuilder.CUIBuilder()
+  BUILDER_INSTANCE.show()
 
 def viewer():
-	import cuiViewer
-	reload(cuiViewer)
-	viewer = cuiViewer.CUIViewer()
-	viewer.show()
+  global VIEWER_INSTANCE
+  reload(cuiViewer)
+  VIEWER_INSTANCE = cuiViewer.CUIViewer()
+  VIEWER_INSTANCE.show()
+
+def killCUI():
+  global BUILDER_INSTANCE, VIEWER_INSTANCE
+
+  if BUILDER_INSTANCE:
+    BUILDER_INSTANCE.close()
+    BUILDER_INSTANCE.deleteLater()
+    BUILDER_INSTANCE = None
+
+  if VIEWER_INSTANCE:
+    VIEWER_INSTANCE.close()
+    VIEWER_INSTANCE.deleteLater()
+    VIEWER_INSTANCE = None
+
+def update():
+  killCUI()
+  import update
+  update.updateFromGitHub()
+  
---
+++ update.py
@@ -0,0 +1,28 @@
+import urllib2
+import zipfile
+import os
+from StringIO import StringIO
+
+def updateFromGitHub():
+  REP_LINK = "https://github.com/falceeffect/MayaCUI/archive/master.zip"
+  EXCLUDE_LIST = [".gitignore", "cui.todo", "README.md"]
+
+  zipData = StringIO(urllib2.urlopen(REP_LINK).read())
+  
+  with zipfile.ZipFile(zipData) as zipFile:
+    for filePath in zipFile.namelist():
+      filename = filePath.split('/')[-1]
+      if not filename or filename in EXCLUDE_LIST:
+        continue
+
+      src = zipFile.open(filePath)
+      dest = open(filename, "wb")
+      dest.write(src.read())
+      src.close()
+      dest.close()
+
+  scriptDir = os.path.dirname(os.path.realpath(__file__)) 
+
+  for filename in os.listdir(scriptDir):
+    if filename.endswith(".pyc"):
+      os.remove(filename)
